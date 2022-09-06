e5b0505010732926d4dd4a7913996b4a1e82bb97
Alex <Yulien.ru@Yandex.ru>
2022-09-06 15:42:54 +0400

    замена + на - и наоборот

---
+++ task32/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.e.WriteLine("Hello, World!");
+
+int [] InvertArray(int[] arr) {
+    for (int i = 0; i < arr.Length; i++){
+        arr [i] *= -1;
+    }
+    return arr;
+}
+int [] array = new int[]{-4, -8, 8, 2};
+foreach (var item in InvertArray(array)){
+    Console.WriteLine(item);
+}
+
