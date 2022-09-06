31e4b8faf8600e3b469883e3c4de9daaf36993fd
Alex <Yulien.ru@Yandex.ru>
2022-09-06 16:17:55 +0400

    присутствует ли заданное число в массиве

---
+++ task33/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+
+bool Contains (int n, int[] arr){
+    // foreach (var item in arr) 
+    // {
+    //     if (item == n){
+    //         return true;
+    //     }
+    // }
+    for (int i = 0; i < arr.Length; i++) // пример с другим методом
+    {
+        if (arr[i] == n){
+            return true;
+        }
+    }
+    return false;
+}
+Console.WriteLine(Contains(5, new int[]{1, 2, 3, 4}));
+Console.WriteLine(Contains(3, new int[]{1, 2, 3, 4}));
